5a7fbf8b59e0af74672ee8be7740747b7350ef71
Duoc <satthukt1232@gmail.com>
2017-08-09 07:56:27 +0700

    Exercise angular

--- AngularJS/duoc-app/src/app/app.component.ts
+++ AngularJS/duoc-app/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, ViewChild,OnInit, AfterContentInit } from '@angular/core';
-import { MyService } from './myservice';
-import { AppDetailComponent } from './detail/app.detail.component';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +7,62 @@ import { AppDetailComponent } from './detail/app.detail.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title:string = 'AngularJS';
-    @ViewChild(AppDetailComponent) detacomp: AppDetailComponent;
-    constructo() {
+  title = 'Member';
+  members: any[] = [];
+  formRegister: any;
+  teams: Array<string>;
 
+  constructor(private fb: FormBuilder) {
+    this.teams = ['FE', 'Ruby', 'PHP'];
+    this.formRegister = this.fb.group({
+      personal: new FormGroup({
+        avatar: new FormControl(),
+        name: new FormControl('',[Validators.required]),
+        birthday: new FormControl()
+      }),
+      company: new FormGroup({
+        team: new FormControl('',[Validators.required]),
+        joined : new FormControl('',[Validators.required])
+      }),
+      skills: new FormControl()
+    });
   }
-    showDetail (trainer: any) {
-     this.detacomp.showmem = trainer; 
-    }
+  ngOnInit() {
+    this.formRegister.controls.company.controls.team.valueChanges.subscribe(
+      (value: string) => {
+        switch (value) {
+          case "Ruby":
+            this.formRegister.setControl('skill', new FormControl('', [Validators.required, this.checkSkillRuby]));
+            break;
+          case "PHP":
+            this.formRegister.setControl('skill', new FormControl('', [Validators.required, this.checkSkillPhp]));
+            break;
+          default:
+            this.formRegister.setControl('skill', new FormControl('', [Validators.required, this.checkSkillFe]));
+            break;
+        }
+      console.log(value);
+      }
+    );
+  }
+  checkSkillRuby = (input: FormControl) => {
+    return this.checkSkillsByTeam(input, 'Ruby'.toLocaleLowerCase());
+  }
+
+  checkSkillFe = (input: FormControl) => {
+    return this.checkSkillsByTeam(input, 'Fe'.toLocaleLowerCase());
+  }
+
+  checkSkillPhp = (input: FormControl) => {
+    return this.checkSkillsByTeam(input, 'PHP'.toLocaleLowerCase())
+  }
+  checkSkillsByTeam(input: FormControl, prerequisite: string) {
+    const ind = input.value.indexOf(prerequisite) < 0;
+    return ind ? { skillInvalid: true } : null
+  }
+
+  addMember() {
+      this.members.push(this.formRegister.value);
+      console.log(this.formRegister.value);
+  }
 }
-
-
--- AngularJS/duoc-app/src/app/app.module.ts
+++ AngularJS/duoc-app/src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpModule} from '@angular/http';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ListMember} from './applist.component';
 import { AppComponent } from './app.component';
-import { AppListComponent } from './applist.component';
-import { AppDetailComponent } from './appdetail.component';
-import { MyService } from './myservice'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AppListComponent,
-    AppDetailComponent
+    AppComponent,ListMember
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [MyService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
